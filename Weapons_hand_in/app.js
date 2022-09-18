bbd0f6440ab1fdf5ba63de59f33cc61fad554416
MadsHaderup <mads@haderup.net>
2022-09-18 20:09:24 +0200

    PUT, PATCH and DELETE  methods implemented for weapons API

--- Weapons_hand_in/app.js
+++ Weapons_hand_in/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const weapons = [
     { id: 1, name: "Pistol"},
@@ -14,6 +15,49 @@ app.get("/weapons", (request, response) => {
 app.get("/weapons/:id", (request, response) => {
     const id = parseInt(request.params.id);
     response.send(weapons.find((weapon) => weapon.id == id));
+})
+
+app.post("/weapons", (request, response) => {
+    weapons.push(request.body);
+    response.status(201).send(`${request.body.name} is saved`);
+})
+
+app.put("/weapons/:id", (request, response) => {
+    const id = parseInt(request.params.id);
+    const body = request.body;
+    
+    let index = weapons.findIndex((weapon) => weapon.id === id);
+    if(index){
+        weapons[index] = body;
+        response.status(200).send(`${request.body.name} is edited`);
+    } else {
+        response.status(404).send("Weapon doesn't exist");
+    }
+})
+
+app.patch("/weapons/:id", (request, response) => {
+    const id = parseInt(request.params.id);
+    const name = request.body.name;
+    
+    let index = weapons.findIndex((weapon) => weapon.id === id);
+    if(index){
+        weapons[index].name = name;
+        response.status(200).send(`${request.body.name} is edited`);
+    } else {
+        response.status(404).send("Weapon doesn't exist");
+    }
+})
+
+app.delete("/weapons/:id", (request, response) => {
+    const id = parseInt(request.params.id);
+    
+    let index = weapons.findIndex((weapon) => weapon.id === id);
+    if(index){
+        weapons.splice(index, 1);
+        response.status(200).send(`${id} is deleted`);
+    } else {
+        response.status(404).send("Weapon doesn't exist");
+    }
 })
 
 app.listen(8080);
