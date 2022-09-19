9cec271fbcec5c0553c87b94107f562625c7a927
MadsHaderup <mads@haderup.net>
2022-09-19 09:49:40 +0200

    PUT, PATCH and DELETE  methods updated for weapons API

--- Weapons_hand_in/app.js
+++ Weapons_hand_in/app.js
@@ -14,12 +14,15 @@ app.get("/weapons", (request, response) => {
 
 app.get("/weapons/:id", (request, response) => {
     const id = parseInt(request.params.id);
-    response.send(weapons.find((weapon) => weapon.id == id));
+    response.send(weapons.find((weapon) => weapon.id === id));
 })
 
 app.post("/weapons", (request, response) => {
-    weapons.push(request.body);
-    response.status(201).send(`${request.body.name} is saved`);
+    let currentId = weapons.length;
+    const weapon = req.body;
+    weapon.id = ++currentId;
+    weapons.push(weapon);
+    response.status(201).send({ data: weapon, message: `Weapon with name: ${request.body.name} is saved`});
 })
 
 app.put("/weapons/:id", (request, response) => {
@@ -29,22 +32,23 @@ app.put("/weapons/:id", (request, response) => {
     let index = weapons.findIndex((weapon) => weapon.id === id);
     if(index){
         weapons[index] = body;
-        response.status(200).send(`${request.body.name} is edited`);
+        response.status(200).send({ data: body.name, message: `Weapon with name: ${request.body.name} is edited`});
     } else {
-        response.status(404).send("Weapon doesn't exist");
+        response.status(404).send({data: undefined, message: `Weapon with id: ${id} doesn't exist`});
     }
 })
 
 app.patch("/weapons/:id", (request, response) => {
     const id = parseInt(request.params.id);
-    const name = request.body.name;
     
-    let index = weapons.findIndex((weapon) => weapon.id === id);
-    if(index){
-        weapons[index].name = name;
-        response.status(200).send(`${request.body.name} is edited`);
+    let index = weapons.findIndex((weapon) => weapon.id === parseInt(request.params.id));
+    if(index !== -1){
+        const foundWeapon = weapons[index];
+        const weaponToUpdate = { ...foundWeapon, ...request.body, id: Number(request.params.id)};
+        weapons[index] = weaponToUpdate;
+        response.status(200).send({ data: weaponToUpdate, message: `Weapon with name: ${weaponToUpdate.name} is edited`});
     } else {
-        response.status(404).send("Weapon doesn't exist");
+        response.status(404).send({ data: undefined, message: `Weapon with id: ${id} doesn't exist`});
     }
 })
 
@@ -53,10 +57,10 @@ app.delete("/weapons/:id", (request, response) => {
     
     let index = weapons.findIndex((weapon) => weapon.id === id);
     if(index){
-        weapons.splice(index, 1);
-        response.status(200).send(`${id} is deleted`);
+        const deletedWeapon = weapons.splice(index, 1);
+        response.status(200).send({ data: deletedWeapon, message: `Weapon with id: ${id} is deleted` });
     } else {
-        response.status(404).send("Weapon doesn't exist");
+        response.status(404).send({ data: undefined, message: "Weapon doesn't exist" });
     }
 })
 
