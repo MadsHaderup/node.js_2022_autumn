3f183382e34262b5426081053ef421453b2ccc7f
MadsHaderup <mads@haderup.net>
2022-09-19 20:38:53 +0200

    Added pokemon website, started clock website

--- 1._Basic_Express/app.js
+++ 1._Basic_Express/app.js
@@ -5,4 +5,7 @@ app.get("/", (request, response) => {
     response.send({ message: "Created my first route. Check"});
 });
 
+//console.log(new Date().toLocaleString());
+
+
 app.listen(8080);
---
+++ 4._Pokemon/public/frontpage.js
@@ -0,0 +1,4 @@
+fetch("/pokemon")
+.then(response => response.json())
+.then(result => console.log(result));
+
